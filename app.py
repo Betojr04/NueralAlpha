b02355a52bb4d7d21fdfcc325ecb10154b3e2706
Sabrina Le <azanirbas@gmail.com>
2025-06-30 19:52:13 -0700

    Implement login/signup front end with SQLite and bcrypt

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from routes.home import home_bp
 from routes.api import api
+from routes.auth import auth_bp
+import os
+from dotenv import load_dotenv
 
 
 def create_app():
     app = Flask(__name__)
+    # load_dotenv()
+    # app.secret_key = os.getenv("SECRET_KEY")
+    app.secret_key = "secret"
 
-    # Register blueprints
     app.register_blueprint(home_bp)
     app.register_blueprint(api)
+    app.register_blueprint(auth_bp)
 
     return app
 
--- routes/home.py
+++ routes/home.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 
 home_bp = Blueprint("home", __name__)
 
-
 @home_bp.route("/")
 def home():
-    return render_template("index.html")
+    user = session.get("user")
+    if not user:
+        return redirect("/login")
+    return render_template("index.html", user=user)
